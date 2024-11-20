07be723c2891847e6b0991c3d73bb7ebff574e84
Yari-i <tipo47181@gmail.com>
2024-11-20 19:11:44 +0500

    star HW

---
+++ src/test/java/pro/sky/shoppingcartdemo/ShoppingCartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.shoppingcartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
